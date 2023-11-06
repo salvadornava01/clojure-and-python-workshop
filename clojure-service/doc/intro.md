a1ef23a23c903824f3d9a7bb8b0e88a0ab1a3268
salvadornava <salvador_nacruz_01@hotmail.com>
2023-11-05 23:10:43 -0600

    Initialized clojure project

---
+++ clojure-service/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-service
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-service/project.clj
@@ -0,0 +1,12 @@
+(defproject clojure-service "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.pedestal/pedestal.service "0.6.0"]
+                 [io.pedestal/pedestal.jetty "0.6.0"]]
+  :main ^:skip-aot clojure-service.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
