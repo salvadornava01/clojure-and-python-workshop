5e60b3994b86bc3c4c2938ef5c7d6634587ad8dc
salvadornava <salvador_nacruz_01@hotmail.com>
2023-11-05 23:12:33 -0600

    First clojure route

--- clojure-service/src/clojure_service/core.clj
+++ clojure-service/src/clojure_service/core.clj
@@ -1,7 +1,27 @@
 (ns clojure-service.core
-  (:gen-class))
+  (:gen-class)
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn response
+[request]
+{:status 200
+ :headers {"Content-Type" "text/html"}
+ :body "<h1>Hello world! :D</h1>"})
+
+(def routes
+  (route/expand-routes
+    #{["/hello" :get response :route-name :hello]}))
+
+(defn create-server
+  []
+  (http/create-server
+    {::http/routes routes
+     ::http/type :jetty
+     ::http/port 8081}))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (http/start (create-server)))
