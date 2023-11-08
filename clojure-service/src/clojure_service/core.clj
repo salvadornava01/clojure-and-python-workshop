8a65b1abf7bf5176df0a7fd5e393deb989dd83f5
salvadornava <salvador_nacruz_01@hotmail.com>
2023-11-08 17:39:29 -0600

    Added structure to blog, and extra data in clojure service api

--- clojure-service/src/clojure_service/core.clj
+++ clojure-service/src/clojure_service/core.clj
@@ -6,10 +6,18 @@
 
 (defn response
 [request]
-{:status 200
- :headers {"Content-Type" "application/json"}
- :body (json/write-str {:text "This text comes from Clojure service! :D"
-                        :objects [{:title "Title 1" :content "Content 1"} {:title "Title 2" :content "Content2"}]})})
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str {:title "This is a simple list of some planets of the Solar System :D"
+                          :articles [{:title "The Earth" 
+                                      :content "The Earth is the 3rd plannet in the Solar System and the place we all live!" 
+                                      :image "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/440px-The_Blue_Marble_%28remastered%29.jpg"} 
+                                     {:title "Mars"
+                                      :content "Mars is the 4th planet in the Solar System, and the place we'll live in the future!" 
+                                      :image "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Mars_Valles_Marineris.jpeg/440px-Mars_Valles_Marineris.jpeg"}
+                                     {:title "Jupiter" 
+                                      :content "Jupiter is the most massive planet in the Solar System!" 
+                                      :image "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Jupiter_New_Horizons.jpg/440px-Jupiter_New_Horizons.jpg"}]})})
 
 (def routes
   (route/expand-routes
--- python-service/home/views.py
+++ python-service/home/views.py
@@ -6,6 +6,8 @@ import requests
 def home_view(request):
     r = requests.get('http://localhost:8081/hello')
     json_response = r.json()
-    text_from_clojure = json_response["text"]
-    context = {'text': text_from_clojure}
-    return render(request, "home.html", context)
+    page_title_from_clojure = json_response['title']
+    articles_from_clojure = json_response['articles']
+    context = {'page_title': page_title_from_clojure,
+               'articles': articles_from_clojure}
+    return render(request, 'home.html', context)
